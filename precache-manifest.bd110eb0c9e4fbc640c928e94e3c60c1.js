f74f850f6576943589c1194e250cf730105c2cc5
Keaton Braithwaite <67600084+keatonBraith@users.noreply.github.com>
2020-10-06 13:02:28 -0600

    Updates

---
+++ precache-manifest.bd110eb0c9e4fbc640c928e94e3c60c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639131a6a42d649f09118b4f7d3adf67",
+    "url": "/kbportfolio/index.html"
+  },
+  {
+    "revision": "cd5ed2e77775f2624811",
+    "url": "/kbportfolio/static/css/main.506f6d66.chunk.css"
+  },
+  {
+    "revision": "4975cabd2b3ea5f15c2e",
+    "url": "/kbportfolio/static/js/2.9ddf0155.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kbportfolio/static/js/2.9ddf0155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5ed2e77775f2624811",
+    "url": "/kbportfolio/static/js/main.8806d2d3.chunk.js"
+  },
+  {
+    "revision": "1ecda426c8f0a164842f",
+    "url": "/kbportfolio/static/js/runtime-main.e1e5a929.js"
+  },
+  {
+    "revision": "4c20da76f0e459df31922152d2dbcf5f",
+    "url": "/kbportfolio/static/media/portfolioback.4c20da76.jpg"
+  }
+]);
